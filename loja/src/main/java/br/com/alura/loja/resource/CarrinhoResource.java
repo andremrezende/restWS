4df0fd277e1fc7dce7b7a48a230a97c61017cada
Andre Rezende <meirezende@hotmail.com>
2016-07-07 10:14:17 -0300

    Url parameter added.

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -2,6 +2,7 @@ package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,9 +15,10 @@ import br.com.alura.loja.dao.CarrinhoDAO;
 @Path("carrinhos")
 public class CarrinhoResource {
 	@GET
+	@Path("{id}")
     @Produces(MediaType.APPLICATION_XML)
-	public String busca () {
+	public String busca (@PathParam("id") long id) {
 		CarrinhoDAO dao = new CarrinhoDAO();
-		return dao.busca(1l).toXML();
+		return dao.busca(id).toXML();
 	}
 }
