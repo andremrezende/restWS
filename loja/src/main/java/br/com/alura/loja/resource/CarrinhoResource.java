2eb64eb509c4d2e1f2214f0611707db05d8e05ee
Andre Rezende <meirezende@hotmail.com>
2016-07-07 13:24:33 -0300

    PUT method added.

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 /**
  * 
@@ -48,6 +50,16 @@ public class CarrinhoResource {
 	public Response removeProduto(@PathParam("id") long id, @PathParam("produtoID") long produtoID) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		carrinho.remove(produtoID);
+		return Response.ok().build();
+	}
+
+	@PUT
+	@Path("{id}/produtos/{produtoID}/quantidade")
+	public Response alteraQuantidadeProduto(String conteudo, @PathParam("id") long id, @PathParam("produtoID") long produtoID) {
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		Produto produto = (Produto) new XStream().fromXML(conteudo);		
+		carrinho.trocaQuantidade(produto);
+		
 		return Response.ok().build();
 	}
 }
