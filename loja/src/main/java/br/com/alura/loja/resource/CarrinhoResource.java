9d5e41372491bf7f08d385f3890c9f61cfcf1543
Andre Rezende <meirezende@hotmail.com>
2016-07-07 11:48:58 -0300

    POST method added.

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,12 +1,17 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.thoughtworks.xstream.XStream;
+
 import br.com.alura.loja.dao.CarrinhoDAO;
+import br.com.alura.loja.modelo.Carrinho;
+
 /**
  * 
  * @author Rezende
@@ -16,9 +21,17 @@ import br.com.alura.loja.dao.CarrinhoDAO;
 public class CarrinhoResource {
 	@GET
 	@Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-	public String busca (@PathParam("id") long id) {
+	@Produces(MediaType.APPLICATION_XML)
+	public String busca(@PathParam("id") long id) {
 		CarrinhoDAO dao = new CarrinhoDAO();
-		return dao.busca(id).toJSON();
+		return dao.busca(id).toXML();
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo) {
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		new CarrinhoDAO().adiciona(carrinho);
+		return "<status>sucesso</status>";
 	}
 }
--- loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,12 +1,16 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.thoughtworks.xstream.XStream;
+
 import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Projeto;
 /**
  * 
  * @author Rezende
@@ -20,5 +24,13 @@ public class ProjetoResource {
 	public String busca (@PathParam("id") long id) {
 		ProjetoDAO dao = new ProjetoDAO();
 		return dao.busca(id).toXML();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo) {
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		new ProjetoDAO().adiciona(projeto);
+		return "<status>sucesso</status>";
 	}
 }
